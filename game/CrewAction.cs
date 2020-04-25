6b5fe10a050f5ed9f3ab34870c18246a97d3b3df
Timofey <tvb060201@gmail.com>
2020-04-25 16:35:07 +0500

    Add CrewMember Constructor

--- game/CrewAction.cs
+++ game/CrewAction.cs
@@ -2,5 +2,8 @@
 {
 	public enum CrewAction
 	{
-	}
+        Staying = 0,
+        Moving = 1,
+        Working = 2,
+    }
 }
--- game/CrewMember.cs
+++ game/CrewMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,31 @@ namespace game
 {
 	class CrewMember
 	{
-		public CrewMember()
+        public int Speed;
+        public Point Coordinates;
+        public string Name;
+        public int HP;
+        public Point Destination;
+        public CrewAction Action;
+        public bool IsAlive;
+        private static readonly List<string> names = new List<string> { "Kolya", "Petya" }
+
+
+		public CrewMember(Point coordinates)
 		{
-            var N;
+            Speed = 1;
+            Coordinates = coordinates;
+            Name = GetName();
+            Destination = coordinates;
+            HP = 100;
+            Action = 0;
+            IsAlive = true;
 		}
+
+        private string GetName()
+        {
+            var random = new Random();
+            return names[random.Next(0, names.Count);];
+        }
 	}
 }
