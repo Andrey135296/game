7dc00c3ba7f4c8eb2c2da8ae8fdf2bd0ebca963b
Timofey <tvb060201@gmail.com>
2020-04-28 19:57:58 +0500

    New Tests

--- game/Tests/RepairingRoom_Should.cs
+++ game/Tests/RepairingRoom_Should.cs
@@ -18,14 +18,48 @@ namespace game
             Assert.IsTrue(ship.Crew.All(c => c.Action == CrewAction.Idle));
             CrewActionsHandler.TickCrew(ship);
 
-            //InterfaceCommands.MoveCrewMember(ship.Crew[1], ship.Cells[5], ship);
-            //CrewActionsHandler.TickCrew(ship);
-            //Assert.AreEqual(ship.Cells[5], ship.Crew[1].Cell);
-
             ship.Rooms[0].CurrentDurability = 10;
             CrewActionsHandler.TickCrew(ship);
             Assert.AreEqual(12, ship.Rooms[0].CurrentDurability);
             Assert.AreEqual(12, ship.SpecialRooms[0].CurrentDurability);
+        }
+
+        [Test]
+        public void TestRepairFullRoom()
+        {
+            var ship = new TestShip(Alignment.Player);
+            Assert.IsTrue(ship.Crew.All(c => c.Action == CrewAction.Idle));
+            CrewActionsHandler.TickCrew(ship);
+
+            CrewActionsHandler.TickCrew(ship);
+            Assert.AreEqual(100, ship.Rooms[0].CurrentDurability);
+            Assert.AreEqual(100, ship.SpecialRooms[0].CurrentDurability);
+        }
+
+        [Test]
+        public void TestRepairMovingCrewMemberRoom()
+        {
+            var ship = new TestShip(Alignment.Player);
+            Assert.IsTrue(ship.Crew.All(c => c.Action == CrewAction.Idle));
+            CrewActionsHandler.TickCrew(ship);
+
+            ship.Rooms[0].CurrentDurability = 10;
+
+            CrewActionsHandler.TickCrew(ship);
+            Assert.AreEqual(12, ship.Rooms[0].CurrentDurability);
+            Assert.AreEqual(12, ship.SpecialRooms[0].CurrentDurability);
+
+            InterfaceCommands.MoveCrewMember(ship.Crew[0], ship.Cells[5], ship);
+            Assert.AreEqual(12, ship.Rooms[0].CurrentDurability);
+            Assert.AreEqual(12, ship.SpecialRooms[0].CurrentDurability);
+
+            CrewActionsHandler.TickCrew(ship);
+            Assert.AreEqual(13, ship.Rooms[0].CurrentDurability);
+            Assert.AreEqual(13, ship.SpecialRooms[0].CurrentDurability);
+
+            CrewActionsHandler.TickCrew(ship);
+            Assert.AreEqual(14, ship.Rooms[0].CurrentDurability);
+            Assert.AreEqual(14, ship.SpecialRooms[0].CurrentDurability);
         }
     }
 }
