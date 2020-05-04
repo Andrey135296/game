4c03d7693e42cd023cbaa4c20ed0f4f4634d7958
Andrey135296 <Andrey135296@yandex.ru>
2020-05-04 16:37:06 +0500

    cleanup

--- game/view/MainForm.cs
+++ game/view/MainForm.cs
@@ -39,27 +39,10 @@ namespace game
 			Controls.Add(StartGrid);
 			AllGrids.Add(StartGrid);
 
-			var rect = new Rectangle(5, 5, 200, 200);
-			Paint += (s, e) => e.Graphics.DrawRectangle(new Pen(Color.Red), rect);
-
 		}
 
 		protected override void OnPaint(PaintEventArgs e)
 		{
-			var graphics = e.Graphics;
-
-			graphics.DrawLine(new Pen(Color.Red, 5), new Point(0, 0), new Point(50, 100));
-			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-			graphics.DrawLine(new Pen(Color.Green, 5), 0, 0, 100, 50);
-			graphics.FillRectangle(Brushes.Green, 100, 100, 100, 100);
-			graphics.DrawString("Some text here", new Font("Arial", 16), Brushes.Black, new Point(0, 250));
-			graphics.DrawString(
-				"Some very long text",
-				new Font("Arial", 16),
-				Brushes.White,
-				new Rectangle(100, 100, 100, 100),
-				new StringFormat { Alignment = StringAlignment.Near, LineAlignment = StringAlignment.Center, FormatFlags = StringFormatFlags.FitBlackBox }
-				);
 		}
 
 		public TableLayoutPanel GenerateMainMenu()
