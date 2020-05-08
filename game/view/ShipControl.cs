a4b1a9bc464dce141769ac9797b831b299fbf713
Andrey135296 <Andrey135296@yandex.ru>
2020-05-08 15:11:22 +0500

    +ship pic + minor changes

--- game/view/ShipControl.cs
+++ game/view/ShipControl.cs
@@ -12,9 +12,19 @@ namespace game
 {
 	public partial class ShipControl : UserControl
 	{
-		public ShipControl()
+		public Ship Ship;
+		public List<RoomControl> Rooms;
+		public ShipControl(Ship ship)
 		{
 			InitializeComponent();
+			Ship = ship;
+			Rooms = Ship.Rooms.Select(r => new RoomControl(r)).ToList();
+			if (Ship is Titan || Ship is TestTitan)
+			{
+				BackgroundImage = new Bitmap("images/Titan.png");
+				BackgroundImageLayout = ImageLayout.Stretch;
+			}
+			BackColor = Color.Transparent;
 		}
 	}
 }
