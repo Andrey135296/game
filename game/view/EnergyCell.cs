c05f94e2952b6ccaef649e9f10efbaef2754581f
Andrey135296 <Andrey135296@yandex.ru>
2020-05-08 22:00:02 +0500

    WeaponEnery dev

--- game/view/EnergyCell.cs
+++ game/view/EnergyCell.cs
@@ -22,7 +22,6 @@ namespace game
 		protected override void OnPaint(PaintEventArgs e)
 		{
 			base.OnPaint(e);
-			//this.BackColor = Color.Transparent;
 			e.Graphics.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0, 0), 2), 2, 2, this.Width-5, this.Height-5);
 			Color cl;
 			switch (state)
--- game/view/WeaponEnergy.cs
+++ game/view/WeaponEnergy.cs
@@ -12,9 +12,37 @@ namespace game
 {
 	public partial class WeaponEnergy : UserControl
 	{
-		public WeaponEnergy()
+		public Weapon Weapon;
+		public int Max, Current;
+		public List<EnergyCell> Cells;
+		public WeaponEnergy(Weapon weapon)
 		{
 			InitializeComponent();
+			this.Weapon = weapon;
+			Max = Weapon.EnergyPrice;
+			Cells = new List<EnergyCell>();
+			var t = new TableLayoutPanel();
+			t.RowCount = 1;
+			t.ColumnCount = Max;
+			for (int i = 0; i < Max; i++)
+				t.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 1.0f / Max));
+			t.BackColor = Color.Transparent;
+			t.Dock = DockStyle.Fill;
+			for (int i = 0; i < Max; i++)
+			{
+				var c = new EnergyCell();
+				c.state = 1;
+				Cells.Add(c);
+				c.Dock = DockStyle.Fill;
+				t.Controls.Add(c, i, 0);
+			}
+			Controls.Add(t);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			Current = Weapon.IsOnline ? Max : 0;
 		}
 	}
 }
